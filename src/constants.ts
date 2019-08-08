5bef6985f10a82941e93d468cae249662fb702be
Marcin J <jerzakm@gmail.com>
2019-08-08 13:10:31 +0200

    env variables loading, constants, typescript config

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const ALLEGRO_URL = `https://allegro.pl`
+export const ALLEGRO_API_URL = `https://api.allegro.pl`
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import * as axios from 'axios'
 import { getAuth } from './Auth/clientCredentials';
 import { base64StringEncode } from './util/crypto';
+import * as envLoad from 'dotenv'
+
+//config
+envLoad.config()
 
 test()
 
 async function test() {
-  const cliendId = ''
-  const clientSecret = ''
+  
+
+
+  const cliendId = process.env.ALLEGRO_CLIENT_ID
+  const clientSecret = process.env.ALLEGRO_CLIENT_SECRET
   
   const g = await getAuth(base64StringEncode(`${cliendId}:${clientSecret}`))
   
