366a61d5e83ae705dbf89fa47734ce01b864126b
Marcin J <jerzakm@gmail.com>
2019-08-08 11:45:07 +0200

    buffer depreciation warning fix

--- src/util/crypto.ts
+++ src/util/crypto.ts
@@ -1,5 +1,5 @@
 export const base64StringEncode = (string: string):string => {
-    let buff = new Buffer(string);
+    let buff = Buffer.from(string);
     let base64data = buff.toString('base64');
     return base64data
 }
